4c93c2c47ae38d65e726289e6b1b70e496b30769
VikasGgit <vikas.college001@gmail.com>
2024-06-18 19:32:45 +0530

    product pages created ,now similar product and Cart button function is going to implemented

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,13 +3,16 @@ import Navbar from './customer/components/Navigation/navbar'
 import Homepage from './customer/Pages/Homepage/Homepage'
 import Footer from './customer/components/Footer/Footer'
 import Product from './customer/components/Product/Product'
+import ProductsDetails from './customer/components/Product/ProductsDetails'
 
 const App = () => {
   return (
     <>
     <Navbar  />
     {/* <Homepage/> */}
-    <Product/>
+    {/* <Product/> */}
+    <ProductsDetails/>
+
     <Footer/>
     </>
   )
